a2fec689934094e821b58244d1505c8d19c3b99c
zoccer99 <leon-gottschild@t-online.de>
2021-10-16 14:11:45 +0200

    updated: backgrounds

--- src/Bambinies.js
+++ src/Bambinies.js
@@ -3,14 +3,19 @@ import BambinisFlyer from "./pictures/Flyer_Bambini.jpg";
 
 
 const Bambinies = () => {
-    return ( 
+    return (
         <div>
-            <blockquote className="blockquote">
-            <p>Unsere Bambinis</p>
+
+            <blockquote class="blockquote ml-3 my-4">
+                <h3 class="">Unsere Bambinis</h3>
+                <footer class="blockquote-footer">Die Zukunft <cite title="Source Title">des Vereins</cite></footer>
             </blockquote>
-            <img src={BambinisFlyer} className="img-fluid"></img>
+            <div className="text-center">
+            
+                <img src={BambinisFlyer} className="rounded img-fluid myThumbnail"></img>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Bambinies;
