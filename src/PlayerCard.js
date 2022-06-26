68a551e5c7dffd416f2d5823b8ae485c51356cd9
zoccer99 <leon-gottschild@t-online.de>
2022-06-26 18:59:42 +0200

    updated: player profile view on mobile

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -8,39 +8,80 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PlayerCard = (props) => {
-  
+  const [isMobile, setMobile] = useState(window.innerWidth < 576);
+
+  const updateViewport = () => {
+    setMobile(window.innerWidth < 576);
+    console.log(isMobile);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateViewport);
+    return () => window.removeEventListener("resize", updateViewport);
+  });
 
   return (
-    <div className="card playerInfoWrapper">
-      <img src={props.img} className="card-img-top playerInfoWrapper" alt="..." />
-      <div className="card-body ">
-        <h4 className="card-text d-flex justify-content-center ">
-         {props.name}
-        </h4>
-      </div>
-      <div className="container py-3 ">
-        <div className="row ">
-          <div className="col-12 col-md-4">
-            <div className="d-flex justify-content-around align-items-center">
-              <FontAwesomeIcon icon={faShoePrints} />
-              <h5 className="m-0">{props.einsätze}</h5>
-            </div>
-          </div>
-          <div className="col-12 col-md-4 ">
-            <div className="d-flex justify-content-around align-items-center ">
-              <FontAwesomeIcon icon={faFutbol} />
-              <h5 className="m-0 ">{props.tore}</h5>
-            </div>
-          </div>
-          <div className="col-12 col-md-4 ">
-            <div className="d-flex justify-content-around align-items-center ">
-              <FontAwesomeIcon icon={faHandshakeSimple} />
-              <h5 className="m-0">{props.assists}</h5>
+    <>
+      {isMobile ? (
+        <div className="d-flex align-items-center justify-content-between playerInfoWrapper mb-1 rounded">
+          <img src={props.img} alt="..." className="img-fluid w-25 rounded" style={{maxWidth: "80px"}} />
+          <h3 className="d-flex align-items-center text-center border-right">{props.name}</h3>
+          <div className=" d-md-block d-flex ">
+            <div className="row ">
+              <div className="col-12 col-md-4">
+                <div className="d-flex justify-content-around align-items-center">
+                  <FontAwesomeIcon icon={faShoePrints} />
+                  <h5 className="m-0">{props.einsätze}</h5>
+                </div>
+              </div>
+              <div className="col-12 col-md-4 ">
+                <div className="d-flex justify-content-around align-items-center ">
+                  <FontAwesomeIcon icon={faFutbol} />
+                  <h5 className="m-0 ">{props.tore}</h5>
+                </div>
+              </div>
+              <div className="col-12 col-md-4 ">
+                <div className="d-flex justify-content-around align-items-center ">
+                  <FontAwesomeIcon icon={faHandshakeSimple} />
+                  <h5 className="m-0">{props.assists}</h5>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="card playerInfoWrapper mb-3">
+          <img src={props.img} className="card-img-top " alt="..." />
+          <div className="card-body">
+            <h4 className="card-text d-flex justify-content-center text-center">
+              {props.name}
+            </h4>
+          </div>
+          <div className="container py-3 d-none d-md-block">
+            <div className="row ">
+              <div className="col-12 col-md-4">
+                <div className="d-flex justify-content-around align-items-center">
+                  <FontAwesomeIcon icon={faShoePrints} />
+                  <h5 className="m-0">{props.einsätze}</h5>
+                </div>
+              </div>
+              <div className="col-12 col-md-4 ">
+                <div className="d-flex justify-content-around align-items-center ">
+                  <FontAwesomeIcon icon={faFutbol} />
+                  <h5 className="m-0 ">{props.tore}</h5>
+                </div>
+              </div>
+              <div className="col-12 col-md-4 ">
+                <div className="d-flex justify-content-around align-items-center ">
+                  <FontAwesomeIcon icon={faHandshakeSimple} />
+                  <h5 className="m-0">{props.assists}</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/teams/FirstTeam.js
+++ src/teams/FirstTeam.js
@@ -12,118 +12,115 @@ import SpielerErik from "../pictures/Spielerprofile/Dobierzin_Erik.JPG";
 import SpielerElias from "../pictures/Spielerprofile/Just_Elias.JPG";
 import SpielerLeon from "../pictures/Spielerprofile/Gottschild_Leon.JPG";
 
-class FirstTeam extends React.Component {
-  randomNumbers(max) {
+const FirstTeam = () => {
+  const randomNumbers = (max) => {
     let temp = Math.floor(Math.random() * max);
     return temp;
-  }
+  };
+  return (
+    <div className="container-fluid w-lg-75">
+      <blockquote className="blockquote">
+        <h1 className="mb-0">Erste Mannschaft</h1>
+        <h2 className="blockquote-footer pt-4 ">
+          Kreisoberliga Saison 2020/2021
+        </h2>
+      </blockquote>
+      <div className="">
+        <ModalImage
+          small={firstTeam}
+          large={firstTeam}
+          className="img-fluid img-thumbnail my-3 "
+          alt="Erste Mannschaft"
+        />
+      </div>
+      <div className="countdownContainer rounded p-4">
+        <h4>Saison 2020/2021</h4>
+        <h5 className="lead">Hinten von links nach rechts:</h5>
+        <p className="">
+          Markus Schumann, Daniel Piehl, Elias Just, Nico Balschun, Luca Jena,
+          Quentin Bernstein, Christopher Pauli, Jonas Höntsch, Andrè Barth
+        </p>
+        <h5 className="lead">Vorn von links nach rechts:</h5>
+        <p>
+          Ingo Grajek, Heiko Seifert, Hartmut Friedrich, Lars Hendrischke,
+          Christian Gebert, Leon Gottschild, Patrick Fuhrmann, Christopher
+          Scholz, Manuel Lages, Ole Friedrich, Paul Mühlmann, Erik Dobierzin
+        </p>
+        <h5 className="lead">Vorn von links nach rechts:</h5>
+        <p>
+          Phillipp Seifert, Claas Schmeißer, Daniel Albert, Etienne Kalkofen,
+          Andreas Ludwig, Christian Protz, Tino Lai, Christoph Freyer
+        </p>
+      </div>
 
-  render() {
-    return (
-      <div className="container-fluid w-lg-75">
-        <blockquote className="blockquote">
-          <h1 className="mb-0">Erste Mannschaft</h1>
-          <h2 className="blockquote-footer pt-4 ">
-            Kreisoberliga Saison 2020/2021
-          </h2>
-        </blockquote>
-        <div className="">
-          <ModalImage
-            small={firstTeam}
-            large={firstTeam}
-            className="img-fluid img-thumbnail my-3 "
-            alt="Erste Mannschaft"
-          />
-        </div>
-        <div className="countdownContainer rounded p-4">
-          <h4>Saison 2020/2021</h4>
-          <h5 className="lead">Hinten von links nach rechts:</h5>
-          <p className="">
-            Markus Schumann, Daniel Piehl, Elias Just, Nico Balschun, Luca Jena,
-            Quentin Bernstein, Christopher Pauli, Jonas Höntsch, Andrè Barth
-          </p>
-          <h5 className="lead">Vorn von links nach rechts:</h5>
-          <p>
-            Ingo Grajek, Heiko Seifert, Hartmut Friedrich, Lars Hendrischke,
-            Christian Gebert, Leon Gottschild, Patrick Fuhrmann, Christopher
-            Scholz, Manuel Lages, Ole Friedrich, Paul Mühlmann, Erik Dobierzin
-          </p>
-          <h5 className="lead">Vorn von links nach rechts:</h5>
-          <p>
-            Phillipp Seifert, Claas Schmeißer, Daniel Albert, Etienne Kalkofen,
-            Andreas Ludwig, Christian Protz, Tino Lai, Christoph Freyer
-          </p>
-        </div>
-
-        <ContributionSite team="first" />
-        <div id="widget1"></div>
-        <script type="text/javascript">
-          new fussballdeWidgetAPI().showWidget('widget1',
-          '02HRFJV13C000000VUM1DNPHVT26GPRA');
-        </script>
-        <div className="container">
-          <div className="row mb-3">
-            <div className="col">
-              <PlayerCard
-                img={SpielerNico}
-                name="Nico Balschun"
-                einsätze={this.randomNumbers(30)}
-                tore={this.randomNumbers(30)}
-                assists={this.randomNumbers(30)}
-              />
-            </div>
-            <div className="col">
-              <PlayerCard
-                img={SpielerElias}
-                name="Elias Just"
-                einsätze={this.randomNumbers(10)}
-                tore={this.randomNumbers(6)}
-                assists={this.randomNumbers(3)}
-              />
-            </div>
-            <div className="col">
-              <PlayerCard
-                img={SpielerErik}
-                name="Erik Dobierzin"
-                einsätze={this.randomNumbers(0)}
-                tore={this.randomNumbers(0)}
-                assists={this.randomNumbers(0)}
-              />
-            </div>
+      <ContributionSite team="first" />
+      <div id="widget1"></div>
+      <script type="text/javascript">
+        new fussballdeWidgetAPI().showWidget('widget1',
+        '02HRFJV13C000000VUM1DNPHVT26GPRA');
+      </script>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <PlayerCard
+              img={SpielerNico}
+              name="Nico Balschun"
+              einsätze={randomNumbers(30)}
+              tore={randomNumbers(30)}
+              assists={randomNumbers(30)}
+            />
           </div>
-          <div className="row">
-            <div className="col">
-              <PlayerCard
-                img={SpielerLeon}
-                name="Leon Gottschild"
-                einsätze={this.randomNumbers(30)}
-                tore={this.randomNumbers(20)}
-                assists={this.randomNumbers(15)}
-              />
-            </div>
-            <div className="col">
-              <PlayerCard
-                img={SpielerEtienne}
-                name="Etienne Kalkofen"
-                einsätze={this.randomNumbers(27)}
-                tore="13"
-                assists="10"
-              />
-            </div>
-            <div className="col">
-              <PlayerCard
-                img={SpielerQuentin}
-                name="Quentin Bernstein"
-                einsätze={this.randomNumbers(3)}
-                tore="0"
-                assists="0"
-              />
-            </div>
+          <div className="col-12 col-sm-4">
+            <PlayerCard
+              img={SpielerElias}
+              name="Elias Just"
+              einsätze={randomNumbers(10)}
+              tore={randomNumbers(6)}
+              assists={randomNumbers(3)}
+            />
+          </div>
+          <div className="col-12 col-sm-4">
+            <PlayerCard
+              img={SpielerErik}
+              name="Erik Dobierzin"
+              einsätze={randomNumbers(0)}
+              tore={randomNumbers(0)}
+              assists={randomNumbers(0)}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-sm-4">
+            <PlayerCard
+              img={SpielerLeon}
+              name="Leon Gottschild"
+              einsätze={randomNumbers(30)}
+              tore={randomNumbers(20)}
+              assists={randomNumbers(15)}
+            />
+          </div>
+          <div className="col-12 col-sm-4">
+            <PlayerCard
+              img={SpielerEtienne}
+              name="Etienne Kalkofen"
+              einsätze={randomNumbers(27)}
+              tore="13"
+              assists="10"
+            />
+          </div>
+          <div className="col-12 col-sm-4">
+            <PlayerCard
+              img={SpielerQuentin}
+              name="Quentin Bernstein"
+              einsätze={randomNumbers(3)}
+              tore="0"
+              assists="0"
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default FirstTeam;
