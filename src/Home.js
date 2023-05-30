518ba34036d0d6cb93d6f37908d07910e5ae9c16
Leon Gottschild <leon-gottschild@t-online.de>
2023-05-30 14:26:42 +0200

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,70 +1,25 @@
-import React from "react";
-import Slideshow from "./Slideshow";
-import MobileGrid from "./MobileGrid";
-import Sponsoren from "./Sponsoren";
-import Countdown from "./Coundown";
-import ContributionSite from "./Blog/ContributionSite";
-import MyImageGallery from "./Blog/MyImageGallery";
+import React from 'react';
+import Slideshow from './Slideshow';
+import MobileGrid from './MobileGrid';
+import Countdown from './Coundown';
+import Sponsoren from './Sponsoren'
 
 function Home(props) {
-  const games = [
-    ["SG Burgwerben", "SV Kretzschau", new Date("December 04,2022 14:00")],
-  ];
+    return (
+        <div className="">
 
-<<<<<<< HEAD
             <Slideshow />
             <MobileGrid />
             <div className="d-flex flex-column align-items-center">
                 <div className="CounConWrapper">
                     <Countdown date={new Date('June 04,2023 15:00')} heimmannschaft="SV Kretzschau" gastmannschaft="SV Eintracht Profen"/>
-                    {/* <ContributionSite /> */}
+                    <ContributionSite />
                     <Sponsoren />
                 </div>
             </div>
-=======
-  const filterGamesForDate = (games) => {
-    const today = new Date();
-    let diffs = {};
->>>>>>> 21a3bb2897500092a9bda80e1919c7e607053a6c
 
-    for (let i in games) {
-      let diff = games[i][2] - today;
-      if (diff > 0) {
-        diffs[games[i]] = diff;
-      }
-    }
-    let values = Object.values(diffs);
-    let min = Math.min(...values);
-    let game = Object.keys(diffs).find((key) => diffs[key] === min);
-    try {
-
-      game = game.split(","); //string to array
-      game[2] = new Date(game[2]);
-      return game; // [heim, gast, zeit]
-    }
-    catch {
-      return undefined;
-    }
-  };
-
-  let nextGame = filterGamesForDate(games);
-  return (
-    <>
-      <Slideshow />
-      <MobileGrid />
-      <div className="d-flex flex-column align-items-center ">
-        <div className="CounConWrapper">
-          <Countdown
-           game = {nextGame}
-          />
-
-          <ContributionSite />
-          <div style={{ height: "100px" }}></div>
-          <Sponsoren />
         </div>
-      </div>
-    </>
-  );
+    );
 }
 
 export default Home;
